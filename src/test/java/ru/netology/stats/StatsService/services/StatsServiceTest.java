71b2023e08ce97fe91eba32219a36e8807ecb7d7
Jenek <Jenek619@gmail.com>
2023-06-23 00:10:06 +0300

    fix2

--- src/test/java/ru/netology/stats/StatsService/services/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsService/services/StatsServiceTest.java
@@ -9,10 +9,10 @@ public class StatsServiceTest {
     public void shouldAllSales() {
         StatsService service = new StatsService();
 
-        long[] AllSales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long[] allsales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
         int expected = 180;
-        long actual = service.allsales(AllSales);
+        long actual = service.allsales(allsales);
 
         Assertions.assertEquals(expected, actual);
     }
